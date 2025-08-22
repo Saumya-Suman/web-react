e8adabe06b5cf4fcbc2d98a65590c9bbd240311b
Saumya Suman <saumyasuman2799@gmail.com>
2025-08-22 17:40:35 +0530

    Day 2

--- App.js
+++ App.js
@@ -1,21 +1,11 @@
-// const parent= React.createElement( //object --> means it is a react element
-//     "div", {id: "parent" }, // pass  attributes
-//     [ React.createElement("div", {id: "child1" }, [
-//      React.createElement("h1", {}, "I'm an h1 tag"),   //children you have  to pass 
-//        React.createElement("h2", {}, "I'm an h2 tag"), 
-//    ]),  
 
-//     React.createElement("div", {id:"child2"}, [
-//         React.createElement("h1", {}, "I'm an h1 tag"),
-//         React.createElement("h2",  {}, "I'm an h2 tag"),
-//         ]),
-    
-// ]);
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent2 = React.createElement("div", {id:"parent"}, [
 React.createElement("div",  {id:"child1"}, [
-React.createElement("h1", {}, "I'm an h1 tag"),
-React.createElement("h2", {}, "I'm an h2 tag"),
+React.createElement("h1", {}, "React by Saumya"),
+React.createElement("h2", {}, "Learning web dev"),
 ]),
 
 React.createElement("div",{id:"child2"}, [
